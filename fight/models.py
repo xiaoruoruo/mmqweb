0a02abdd6cfa392538738a16d344dae3418e616b
Xinruo Sun <xiaoruoruo@gmail.com>
2012-05-15 13:24:47 +0800

    download apk; simple GameRecord string representation

--- fight/models.py
+++ fight/models.py
@@ -1,6 +1,13 @@
 from django.db.models import *
+import json
 
 class GameRecord(Model):
     ip = CharField(max_length=40)
     json = CharField(max_length=1000)
     
+    def __unicode__(self):
+        o = json.loads(self.json)
+        if o['ver'] >= 1:
+            duration = (o['endTime'] - o['readyTime'])/1000.0/60.0
+        return "%f min" % duration
+
