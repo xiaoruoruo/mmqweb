dc95fb115c2ea53c9c5ed4fdec5783932c1d9ace
Xinruo Sun <xiaoruoruo@gmail.com>
2012-06-06 13:12:02 +0800

    reverse matches

--- game/views.py
+++ game/views.py
@@ -61,7 +61,7 @@ def tournament_edit(request, tid, text_status="", addmatch_status="", match_text
 
 def matches(request, mgid):
     mg = MatchGroup.objects.get(id=mgid)
-    matches = sorted(mg.match_set.all())
+    matches = sorted(mg.match_set.all(), reverse=True)
     return render_to_response("matches.html", {'mg':mg,'matches':matches}, RequestContext(request))
 
 @permission_required('game.change_tournament')
