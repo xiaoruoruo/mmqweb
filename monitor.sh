ffdb711c3c8bf7a258e0fb695a59316f544c8d63
Xinruo Sun <xiaoruoruo@gmail.com>
2013-01-02 12:43:53 +0800

    monitor: fix, full path

--- monitor.sh
+++ monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 wget --timeout=3 --tries=1 http://127.0.0.1:8557/ -O /dev/null
 if [ $? -ne 0 ];then
-  PID=$(<"gunicorn.pid")
+  PID=$(<"/home/apex/mmqweb/gunicorn.pid")
   echo "Restarting $PID"
   kill -HUP $PID
 fi
